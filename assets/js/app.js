d2d66fcf95d5e7d737e9a21f716b708bd548b1d8
ChiaraPapagni <chiaraiors@gmail.com>
2021-11-02 11:51:49 +0100

    stampato array in pagina

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,14 @@
 //istanza vue.js
 const app = new Vue({
     el: '#app',
-    data: {},
+    data: {
+        tasks: [
+            'Task 1',
+            'Task 2',
+            'Task 3',
+            'Task 4'
+        ],
+    },
     methods: {},
 });
+
